9619c5c305d9c2fd4e1ef5d52a327312ba746520
Iswarya Parupudi <iswarya@Iswaryas-MBP.home>
2018-02-22 14:46:38 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <h1 class="ui dividing centered header">Vue.js Todo App</h1>
+    <div class='ui three column centered grid'>
+      <div class='column'>
+        <todo-list v-bind:todos="todos"></todo-list>
+        <create-todo v-on:create-todo="createTodo"></create-todo>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  // Tutorial from : https://scotch.io/tutorials/build-a-to-do-app-with-vue-js-2
+  import TodoList from './components/TodoList'
+  import CreateTodo from './components/CreateTodo'
+
+  export default {
+    name: 'App',
+    components: {
+      TodoList,
+      CreateTodo
+    },
+    data(){
+      return {
+        todos: [
+          {
+            title: 'Todo A',
+            project: 'Project A',
+            done: false
+          },
+          {
+            title: 'Todo B',
+            project: 'Project B',
+            done: true
+          },
+          {
+            title: 'Todo C',
+            project: 'Project C',
+            done: false
+          },
+          {
+            title: 'Todo D',
+            project: 'Project D',
+            done: true
+          }
+        ]
+      }
+    },
+    methods: {
+      createTodo(newTodo) {
+        this.todos.push(newTodo);
+        alert('Success! To-Do created!');
+      },
+    }
+  }
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class='ui basic content center aligned segment'>
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <todo v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" :todo.sync="todo"></todo>
+  </div>
+
+</template>
+
+<script type = "text/javascript">
+
+  import Todo from './Todo';
+
+  export default {
+        name: "todo-list",
+        props: ['todos'],
+        components: {
+          Todo,
+        },
+        methods: {
+          deleteTodo(todo) {
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos.splice(todoIndex, 1);
+            alert('Your To-Do has been deleted.');
+          },
+          completeTodo(todo) {
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos[todoIndex].done = true;
+            alert('To-Do completed!');
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
